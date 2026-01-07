c1a5c7f23655c04de9a183283e99445bc5bec1de
AliAl-ojeely <alialojeely@gmail.com>
2026-01-07 09:43:30 +0300

    create Snake Launch Screen class

--- SnakeGame/SnakeGame.cpp
+++ SnakeGame/SnakeGame.cpp
@@ -1,28 +1,9 @@
 ﻿#include <SFML/Graphics.hpp>
 #include "Snake.h"
+#include "SnakeLaunchScreen.h"
 
-int main() {
-
-    sf::RenderWindow window(sf::VideoMode({ 1270, 720 }), "Snake Game SFML 3.0");
-    window.setFramerateLimit(60);
-
-    Snake snake;
-
-    while (window.isOpen()) {
-    
-        while (const std::optional event = window.pollEvent()) {
-            if (event->is<sf::Event::Closed>()) {
-                window.close();
-            }
-        }
-
-        snake.handleInput();
-        snake.update();
-
-        window.clear();
-        snake.render(window);
-        window.display();
-    }
-
+int main()
+{
+    SnakeLaunchScreen::ShowLaunchScreen();
     return 0;
 }
---
+++ SnakeGame/SnakeLaunchScreen.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include "Snake.h"
+
+class SnakeLaunchScreen
+{
+public:
+
+	static void ShowLaunchScreen()
+	{
+        sf::RenderWindow window(sf::VideoMode({ 1270, 720 }), "Snake Game SFML 3.0");
+        window.setFramerateLimit(60);
+
+        Snake snake;
+
+        while (window.isOpen()) {
+
+            while (const std::optional event = window.pollEvent()) {
+                if (event->is<sf::Event::Closed>()) {
+                    window.close();
+                }
+            }
+
+            snake.handleInput();
+            snake.update();
+
+            window.clear();
+            snake.render(window);
+            window.display();
+        }
+	}
+};
+
